6e204d11423929ef079442f0f99a775f120a99e5
okaki1999 <okakilikecat@gmail.com>
2024-07-05 16:21:07 +0900

    first commit

---
+++ app/api/chat-gpt-api/route.ts
@@ -0,0 +1,36 @@
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+import OpenAI from 'openai'
+
+
+export async function POST(req: Request) {
+    try {
+        const { messages } = await req.json()
+
+        const initialMessages = messages.slice(0, -1)
+        const currentMessage = messages[messages.length - 1]
+        const openai = new OpenAI({
+        apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY || '',
+        })
+
+        const response = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo-1106',
+            stream: true,
+            max_tokens: 150,
+            messages: [
+            ...initialMessages,
+            {
+                ...currentMessage
+            },
+            ],
+        })
+        const stream = OpenAIStream(response);
+        return new StreamingTextResponse(stream);
+
+    } catch (error) {
+        // エラーレスポンスを返す
+        return new Response(JSON.stringify({ error: 'API request failed' }), {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+        });
+    }
+}
